444b36f321df379da45f1890147cde990a05b2f4
Benjamin Schenker <benjamin.schenker@ge.com>
2016-05-27 14:49:11 -0400

    add new directive test and update name test to be more consice

--- public/scripts/src/directives/qcChatbox.spec.js
+++ public/scripts/src/directives/qcChatbox.spec.js
@@ -1,9 +1,9 @@
 /* eslint-env jasmine */
 /* eslint func-names: "off", no-param-reassign: "off"*/
 /* global inject */
-describe('Unit testing Name Directive', () => {
+describe('Unit testing chatBox Directive', () => {
   let $compile;
-  let $rootScope;
+  let $rootscope;
   let $httpBackend;
 
   // Load the myApp module, which contains the directive
@@ -14,21 +14,21 @@ describe('Unit testing Name Directive', () => {
     inject((_$compile_, _$rootScope_, _$httpBackend_) => {
       // The injector unwraps the underscores (_) from around the parameter names when matching
       $compile = _$compile_;
-      $rootScope = _$rootScope_;
+      $rootscope = _$rootScope_;
       // Directive loads a template with http so we mock it out using the whenGET function below
       $httpBackend = _$httpBackend_;
     });
   });
 
   it('Replaces the element with the appropriate partial view', () => {
-    $httpBackend.whenGET('/views/qc-name')
-      .respond('What is your name?');
+    $httpBackend.whenGET('/views/qc-chatbox')
+      .respond('Chat Here');
     // Compile a piece of HTML containing the directive
-    const element = $compile('<qc-name></qc-name>')($rootScope);
+    const element = $compile('<qc-chatbox></qc-chatbox>')($rootscope);
     // fire all the watches and return all the http requests
-    $rootScope.$digest();
+    $rootscope.$digest();
     $httpBackend.flush();
     // Check that the compiled element contains the templated content
-    expect(element.html()).toContain('What is your name?');
+    expect(element.html()).toContain('Chat Here');
   });
 });
