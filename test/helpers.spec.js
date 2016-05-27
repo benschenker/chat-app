8fe8a9ac4df032bd67f205ab553d9226fa5c26b1
Benjamin Schenker <benjamin.schenker@ge.com>
2016-05-27 16:09:16 -0400

    Add back end test for addNextChatterToChat

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -19,4 +19,52 @@ describe('Helpers Module', () => {
       expect(helpers.isVisitor('432zx')(visitor)).to.eql(false);
     });
   });
+  describe('addNextChatterToChat()', () => {
+    const visitor0 = {
+      id: '0',
+      name: 'visitor0',
+    };
+    const visitor1 = {
+      id: '1',
+      name: 'visitor1',
+    };
+    let state;
+
+    it('takes a visitor from the queue and makes them the visitorChatting', () => {
+      state = {
+        queue: [
+          visitor1,
+        ], // array of visitor objects
+        visitorChatting: visitor0, // a visitor object consisting of a name and an id
+        operator: '123asdf', // the socket id of the logged in operator
+      };
+      const newState = helpers.addNextChatterToChat(state);
+      expect(newState.queue.length).to.eql(0);
+      expect(newState.visitorChatting).to.eql(visitor1);
+    });
+
+    it('empties the visitorChatting when the queue is empty', () => {
+      state = {
+        queue: [], // array of visitor objects
+        visitorChatting: visitor0, // a visitor object consisting of a name and an id
+        operator: '123asdf', // the socket id of the logged in operator
+      };
+      const newState = helpers.addNextChatterToChat(state);
+      expect(newState.queue.length).to.eql(0);
+      expect(newState.visitorChatting).to.eql({});
+    });
+
+    it('empties the visitorChatting when there is no operator', () => {
+      state = {
+        queue: [
+          visitor1,
+        ], // array of visitor objects
+        visitorChatting: visitor0, // a visitor object consisting of a name and an id
+        operator: undefined, // the socket id of the logged in operator
+      };
+      const newState = helpers.addNextChatterToChat(state);
+      expect(newState.queue.length).to.eql(1);
+      expect(newState.visitorChatting).to.eql({});
+    });
+  });
 });
